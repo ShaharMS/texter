a8bc71597cf3f9c333c3d71f82c3c9b017537264
ShaharMS <shaharmsecond@gmail.com>
2022-02-08 14:56:31 +0200

    Create WordWrapper.hx

---
+++ src/texter/flixel/_internal/WordWrapper.hx
@@ -0,0 +1,12 @@
+package texter.flixel._internal;
+
+class WordWrapper {
+    
+    public static function wrap(text:String):String {
+        return "";
+    }
+
+    public static function explodeString(text:String):Array<String> {
+        return [];
+    }
+}
